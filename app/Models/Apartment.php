026ce610dbf4bd88a5f4b1389809220e1ee27ab7
Ga <gabri.magni.97@gmail.com>
2022-07-01 11:18:52 +0200

    messages table and relation

--- app/Models/Apartment.php
+++ app/Models/Apartment.php
@@ -9,4 +9,8 @@ class Apartment extends Model
     public function user (){
         return $this-> belongsTo('App\User'); 
     }
+
+    public function messages (){
+        return $this-> hasMany('App\Models\Message'); 
+    }
 }
