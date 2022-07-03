04f7c29a063030c41d58f197507e5554e44ff18e
dibss <ludovicad01@gmail.com>
2022-07-03 10:43:37 +0200

    added apartment's details

--- resources/views/admin/apartments/show.blade.php
+++ resources/views/admin/apartments/show.blade.php
@@ -10,12 +10,19 @@
         <img src="{{ asset('storage/' . $post->image) }}" class="card-img-top mt-2" alt="{{ $post->title }}">
         <p class="card-text">{{ $apartment->description }}</p>
 
-        <!-- <span>Services:</span>
+        <div>
+          <span class="card-text">Rooms: {{ $apartment->rooms }}</span>
+          <span class="card-text">Bathrooms: {{ $apartment->bathrooms }}</span>
+          <span class="card-text">Beds: {{ $apartment->beds }}</span>
+          <span class="card-text">Mq: {{ $apartment->Mq }}</span>
+        </div>
+
+        <span>Services:</span>
         @forelse($apartment->services as $service)
           <span  class="badge badge-pill">{{$service->name}}</span>
         @empty
           <h3>There are no reported services</h3>
-        @endforelse -->
+        @endforelse
 
         <div>
           <button type="button" class="btn btn-primary my-2"><a class="text-white" href="{{route('admin.apartments.edit', $apartment->id)}}">Update</a></button>
