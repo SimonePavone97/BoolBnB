d20c644a027ae386574b359c8d4cd8feb1b2981d
ValentinoDeStefano98 <valentino.destefano46@gmail.com>
2022-07-03 10:25:17 +0200

    Sponsorship seeder

--- database/seeds/SponsorshipSeeder.php
+++ database/seeds/SponsorshipSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Models\Sponsorship;
 
 class SponsorshipSeeder extends Seeder
 {
@@ -11,6 +12,22 @@ class SponsorshipSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $sponsorship = new Sponsorship();
+        $sponsorship->name = 'Plus';
+        $sponsorship->price = 2.99;
+        $sponsorship->duration = 24;  
+        $sponsorship->save();
+
+        $sponsorship = new Sponsorship();
+        $sponsorship->name = 'Premium';
+        $sponsorship->price = 5.99;
+        $sponsorship->duration = 72;  
+        $sponsorship->save();
+
+        $sponsorship = new Sponsorship();
+        $sponsorship->name = 'Gold';
+        $sponsorship->price = 9.99;
+        $sponsorship->duration = 144;  
+        $sponsorship->save();
     }
 }
