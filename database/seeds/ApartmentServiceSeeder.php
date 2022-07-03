c7b570807ec1bb6e70d619af3edc9693c0b67866
dibss <ludovicad01@gmail.com>
2022-07-03 13:24:58 +0200

    created pivot table apartment_services seeder

---
+++ database/seeds/ApartmentServiceSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\ApartmentService;
+use App\Models\Service;
+use App\Models\Apartment;
+use Illuminate\Support\Arr;
+
+class ApartmentServiceSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $service_id = Service::pluck('id')->toArray();
+        $apartment_id = Apartment::pluck('id')->toArray();
+        
+        for ($i = 0; $i < 10; $i++) {
+            $apartment_service = new ApartmentService();
+
+            $apartment_service->service_id = Arr::random($service_id);
+            $apartment_service->apartment_id = Arr::random($apartment_id);
+
+            $apartment_service->save();
+
+        }
+    }
+}
