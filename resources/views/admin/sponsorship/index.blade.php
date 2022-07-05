8af56e325348bfc511e1a31820ad5488915f0171
ValentinoDeStefano98 <valentino.destefano46@gmail.com>
2022-07-05 19:30:11 +0200

    added payment section

--- resources/views/admin/sponsorship/index.blade.php
+++ resources/views/admin/sponsorship/index.blade.php
@@ -24,7 +24,26 @@
                 </div>               
             </div>
         </div>
-        <button type="submit" class="btn btn-success">Acquista</button>
+        <div id="dropin-container"></div>
+            <button id="submit-button" type="submit" class="btn btn-success">Acquista</button>
     </form>
 </div>
 @endsection
+
+<script src="https://js.braintreegateway.com/web/dropin/1.33.2/js/dropin.js"></script>
+<script>
+    var button = document.querySelector('#submit-button');
+
+braintree.dropin.create({
+  authorization: 'sandbox_g42y39zw_348pk9cgf3bgyw2b',
+  selector: '#dropin-container'
+}, function (err, instance) {
+  button.addEventListener('click', function () {
+    instance.requestPaymentMethod(function (err, payload) {
+      // Submit payload.nonce to your server
+    });
+  })
+});
+</script>
+
+
