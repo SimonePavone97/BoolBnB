93da8a53e10ea2d35e7fc1c539c135d9e1becf65
Ga <gabri.magni.97@gmail.com>
2022-07-05 16:10:08 +0200

    apartment page vue

---
+++ resources/js/components/pages/ApartmentDetailPage.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        ciaooooooooooooooo
+        <h2>{{ apartment.title }}</h2>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'ApartmentDetailPage',
+
+    data() {
+        return {
+            apartment: [],
+            isError: false,
+        }
+    },
+
+    methods: {
+        getApartment() {
+            // il routing mi permette di usare $route che mi da determinate info (this.$route è parte di vue)
+            axios.get(`http://127.0.0.1:8000/api/apartments/${this.$route.params.id}`)
+                .then((res) => {
+                    console.log(res.data);
+                    this.post = res.data;
+                }).catch((err) => {
+                    console.log(err);
+                    this.isError = true;
+                });
+        },
+        mounted() {
+            this.getApartment();
+            console.log(this.$route);
+        },
+    },
+}
+
+
+
+</script>
+
+<style scoped>
+</style>
