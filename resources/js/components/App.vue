a1d5f0c2bbd8932a94992a90713cb29414f2b31a
dibss <ludovicad01@gmail.com>
2022-07-07 21:14:24 +0200

    added reset

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -17,5 +17,18 @@ export default {
 </script>
 
 <style lang="scss">
-    
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+li{
+    list-style-type: none;
+}
+
+a{
+    text-decoration: none;
+}
+
 </style>
