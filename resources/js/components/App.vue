64b7a635697c17d89003b2b4a3f0a8e1b423d736
dibss <ludovicad01@gmail.com>
2022-07-07 17:51:24 +0200

    fix sintax

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,42 +1,23 @@
 <template>
     <div>
-<<<<<<< HEAD
-        <HomePage/>
-        <div class="container">
-            <ApartmentsComp/>
-        </div>
-=======
         <main>
             <div class="container">
-                <router-view></router-view>
+            <HomePage/>
+            <router-view></router-view>
             </div>
         </main>
->>>>>>> a3d30ec979559a23091758eb66dc4011a9265d71
     </div>
 </template>
 
 <script>
-<<<<<<< HEAD
-    
-    import ApartmentsComp from './pages/ApartmentsComp.vue';
-    import HomePage from './pages/HomePage.vue';
-    
-    export default {
-        name: "App",
-        components: {
-            ApartmentsComp,
-            HomePage
-}
-=======
-    import HomePage from './pages/HomePage.vue';
+import HomePage from './pages/HomePage.vue';
 
-    export default {
-        name: "App",
-        components: {
-            HomePage
-        }
->>>>>>> a3d30ec979559a23091758eb66dc4011a9265d71
+export default {
+    name: "App",
+    components: {
+        HomePage
     }
+}
 </script>
 
 <style scoped>
