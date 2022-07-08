7569f03a800441fae985d1b64a6eb62b121ad406
dibss <ludovicad01@gmail.com>
2022-07-08 10:00:37 +0200

    updated in italian

--- resources/views/admin/user/show.blade.php
+++ resources/views/admin/user/show.blade.php
@@ -2,24 +2,25 @@
 
 @section('content')
 <div class="container">
+    <h2 class="text-center py-3">Dati utente</h2>
     <div class="card" style="width: 36rem;">
         <div class="card-body">
 
             <div>
-                <span class="card-text">Name: {{ $user->first_name }}</span>
+                <span class="card-text">Nome: {{ $user->first_name }}</span>
             </div>
             <div>
-                <span class="card-text">Last name: {{ $user->last_name }}</span>
+                <span class="card-text">Cognome: {{ $user->last_name }}</span>
+            </div>
+            <div>
+                <span class="card-text">Compleanno: {{ $user->birth_date }}</span>
             </div>
             <div>
                 <span class="card-text">Email: {{ $user->email }}</span>
             </div>
-            <div>
-                <span class="card-text">Birthday: {{ $user->birth_date }}</span>
-            </div>
 
             <div>
-                <button type="button" class="btn btn-primary my-2"><a class="text-white" href="{{route('admin.user.edit', $user->id)}}">Edit</a></button>
+                <button type="button" class="btn btn-primary my-2"><a class="text-white" href="{{route('admin.user.edit', $user->id)}}">Modifica</a></button>
             </div>
         </div>
     </div>
