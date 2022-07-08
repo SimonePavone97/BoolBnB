3f86767cca6689e339a05d18ac4c0314d37c422f
dibss <ludovicad01@gmail.com>
2022-07-08 22:25:07 +0200

    public updates

--- public/js/front.js
+++ public/js/front.js
@@ -19370,7 +19370,7 @@ var router = new vue_router__WEBPACK_IMPORTED_MODULE_1__["default"]({
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/valentinodestefano/Desktop/BoolBnB/resources/js/front.js */"./resources/js/front.js");
+module.exports = __webpack_require__(/*! C:\Users\Ludovica\Desktop\corso_boolean\esercizi\BoolBnB\resources\js\front.js */"./resources/js/front.js");
 
 
 /***/ })
