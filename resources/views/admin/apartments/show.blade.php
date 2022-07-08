0a3de00d5ee4ccc8f46735b1309bec27313f3781
dibss <ludovicad01@gmail.com>
2022-07-08 11:40:12 +0200

    translated and responsive classes

--- resources/views/admin/apartments/show.blade.php
+++ resources/views/admin/apartments/show.blade.php
@@ -3,35 +3,47 @@
 @section('content')
   <div class="container">
 
-    <div class="card" style="width: 36rem;">
-      <div class="card-body">
-        <h5 class="card-title">{{ $apartment->title }}</h5>
+    <h2 class="text-center py-3">Dettagli appartamento</h2>
 
-        <img src="{{ asset('storage/' . $apartment->image) }}" class="card-img-top mt-2" alt="{{ $apartment->title }}">
-        <p class="card-text">{{ $apartment->description }}</p>
+    <div class="card">
+      <div class="card-body d-flex flex-wrap">
 
-        <div>
-          <span class="card-text">Rooms: {{ $apartment->rooms }}</span>
-          <span class="card-text">Bathrooms: {{ $apartment->bathrooms }}</span>
-          <span class="card-text">Beds: {{ $apartment->beds }}</span>
-          <span class="card-text">Mq: {{ $apartment->mq }}</span>
+        <div class="col-12">
+          <h5 class="card-title font-weight-bold">{{ $apartment->title }}</h5>
         </div>
 
-        <span>Services:</span>
-        <ul>
-        @forelse($apartment->services as $service)
-          <li>{{$service->name}}</li>
-        @empty
-          <li>There are no reported services</li>
-        @endforelse
-        </ul>
+        <div class="col-12">
+          <img src="{{ asset('storage/' . $apartment->image) }}" class="card-img-top mt-2" alt="{{ $apartment->title }}">
+        </div>
 
-        <div>
+        <div class="col-12">
+          <p class="card-text">{{ $apartment->description }}</p>
+        </div>
+
+        <div class="col-12">
+          <span class="card-text"><span class="font-weight-bold">Stanze: </span>{{ $apartment->rooms }} - </span>
+          <span class="card-text"><span class="font-weight-bold">Bagni:</span> {{ $apartment->bathrooms }} - </span>
+          <span class="card-text"><span class="font-weight-bold">Letti: </span>{{ $apartment->beds }} - </span>
+          <span class="card-text"><span class="font-weight-bold">Mq: </span>{{ $apartment->mq }}</span>
+        </div>
+
+        <div class="col-12">
+          <span>Servizi:</span>
+          <ul>
+          @forelse($apartment->services as $service)
+            <li>{{$service->name}}</li>
+          @empty
+            <li>Non sono segnalati servizi</li>
+          @endforelse
+          </ul>
+        </div>
+
+        <div class="col-12">
           <button type="button" class="btn btn-primary my-2"><a class="text-white" href="{{route('admin.apartments.edit', $apartment->id)}}">Update</a></button>
           <form action="{{ route('admin.apartments.destroy', $apartment->id) }}" method="post" class="delete-form" data-title="{{ $apartment->title }}">
             @csrf
             @method('DELETE')
-            <button type='submit' class="btn btn-primary">Delete</button>
+            <button type='submit' class="btn btn-danger">Delete</button>
           </form>
           <button type="button" class="btn btn-secondary my-2"><a class="text-white" href="{{route('admin.messages.index', $apartment->id)}}">View messages</a></button>
         </div>
