b9f57dfed8194fbfba095354e3bbea0a468f3f31
dibss <ludovicad01@gmail.com>
2022-07-08 09:52:49 +0200

    updated in italian and added responsive classes

--- resources/views/admin/apartments/create.blade.php
+++ resources/views/admin/apartments/create.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 
   <div id="create-apartment">
-    <div class="container-sm my-4">
-      <h2 class="text-center">Inserisci un nuovo appartamento</h2>
+    <div class="container my-4">
+      <h2 class="text-center py-3">Inserisci un nuovo appartamento</h2>
     
       @if( $errors->any() )
         <div>
@@ -16,31 +16,31 @@
         </div>
       @endif
     
-      <form action=" {{route('admin.apartments.store')}}" method="POST" enctype="multipart/form-data">
+      <form action=" {{route('admin.apartments.store')}}" method="POST" enctype="multipart/form-data" class="d-flex flex-wrap">
         @csrf
     
-        <div class="form-group col-6">
+        <div class="form-group col-12 col-md-6">
           <label for="title">Titolo annuncio</label>
           <input type="text" name="title" class="form-control" id="title" required>
         </div>
-        
-        <div class="form-group col-6">
-          <label for="description">Descrizione</label>
-          <input type="text" name="description" class="form-control" id="description" required>
+
+        <div class="form-group col-12 col-md-6">
+          <label for="address">Indirizzo:</label>
+          <input type="text" name="address" class="form-control" id="address" required>
         </div>
 
         <div class="form-group col-6">
-          <label for="rooms">Rooms:</label>
+          <label for="rooms">Stanze:</label>
           <input type="number" name="rooms" min="0" class="form-control" id="rooms" required>
         </div>
 
         <div class="form-group col-6">
-          <label for="bathrooms">Bathrooms:</label>
+          <label for="bathrooms">Bagni:</label>
           <input type="number" name="bathrooms" min="0" class="form-control" id="bathrooms" required>
         </div>
 
         <div class="form-group col-6">
-          <label for="beds">Beds:</label>
+          <label for="beds">Letti:</label>
           <input type="number" name="beds" min="0" class="form-control" id="beds" required>
         </div>
 
@@ -49,19 +49,20 @@
           <input type="number" name="mq" min="0" class="form-control" id="mq" required>
         </div>
 
-        <div class="form-group col-6">
-          <label for="address">Address:</label>
-          <input type="text" name="address" class="form-control" id="address" required>
+        <div class="form-group col-12">
+          <label for="description">Descrizione:</label>
+          <textarea class="form-control" name="description" id="description" rows="3" required></textarea>
         </div>
     
         <div class="form-group col-6">
-          <label for="image">Image:</label>
+          <label for="image">Immagine:</label>
           <input type="file" name="image" id="image" class="form-control-file">
         </div>
     
-        <div class="form-group">
+        <label for="services" class="col-12">Servizi:</label>
+        <div class="form-group d-flex flex-wrap pl-3">
           @foreach ( $services as $service )
-            <div class="form-check-form-check-inline">
+            <div class="form-check-form-check-inline col-6 col-md-4">
               <input class="form-check-input" type="checkbox" value="{{$service->id}}" id="service-{{$service->id}}" name="services[]">
               <label for="service-{{$service->id}}" class="form-check-label">{{ $service->name }}</label>
             </div>
@@ -69,7 +70,7 @@
         </div>
     
         <div class="col-12">
-          <button type="submit" class="btn btn-primary">Create</button>
+          <button type="submit" class="btn btn-primary">Crea</button>
         </div>
       </form>
     </div>
