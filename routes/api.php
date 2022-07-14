ebad9b351b0a3e154d97234061ef1d01e16260c4
ValentinoDeStefano98 <valentino.destefano46@gmail.com>
2022-07-14 10:34:31 +0200

    added api's route

--- routes/api.php
+++ routes/api.php
@@ -21,4 +21,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::namespace('Api')->group( function(){
     Route::get('/apartments', 'ApartmentController@index');
     Route::get('/apartments/{id}', 'ApartmentController@show');
+    Route::get('/sponsored/apartments', 'ApartmentController@sponsored');
 });
