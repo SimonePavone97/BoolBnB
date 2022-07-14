8f3346563d3fef5ce31f6c19380516f2427c0342
Simone Pavone <simonebest56@gmail.com>
2022-07-14 16:08:34 +0200

    Risolto?

--- resources/js/components/pages/HomePage.vue
+++ resources/js/components/pages/HomePage.vue
@@ -1,29 +1,9 @@
 <template>
-    <div>
-
-
-        <div class="row bg-dark mb-4">
-            <div class="col-12 ">
-                <div class="d-flex justify-content-around align-items-center">
-                    <img src="../../../images/logo.png" alt="Logo BoolBnB" width="180px">
-
-                    <SearchComp @searchFunction="search" />
-                </div>
+    <div id="container-apartments">banana
+        <div class="d-flex align-content-center">
+            <div class="mx-3 col-4">
+                <img src="../../../images/logo.png" alt="logo-Airbnb" width="150px">
             </div>
-<<<<<<< HEAD
-
-
-        </div>
-
-
-
-
-        <div class="container">
-            <div class="row my-3">
-
-                <div class="card col-lg-3 col-md-4 col-sm-12 marg" v-for="apartment in apartmentsArr"
-                    :key="apartment.id" v-show="!searchStatus">
-=======
             <div class="col-4">
                 <SearchComp @searchFunction="search" />
             </div>
@@ -38,7 +18,6 @@
                 <div class="card d-flex justify-content-center align-items-center apartment-card"
                     v-for="(apartment,index) in sponsoredApartmentsArr" :key="index"
                     :apartment="apartment" v-show="!searchStatus">
->>>>>>> cebbd33a4927e279b2c814ab9870c783eb109951
                     <router-link :to="{name: 'apartment-detail', params: {id: apartment.id}}" class="text-dark">
                         <img class="card-img-top" :src="apartment.image" alt="Card image cap">
                         <div class="card-body">
@@ -49,24 +28,6 @@
                                 <span class="card-text">Bagni: {{apartment.bathrooms}}</span>
                                 <span class="card-text">Mq: {{apartment.mq}}</span>
                             </div>
-<<<<<<< HEAD
-                        </div>
-                    </router-link>
-                </div>
-
-                <div class="card d-flex justify-content-center align-items-center apartment-card marg"
-                    v-for="apartment in searchedApartmentsArr" :key="apartment.id" v-show="searchStatus">
-                    <router-link :to="{name: 'apartment-detail', params: {id: apartment.id}}">
-                        <img class="card-img-top" :src="apartment.image" alt="Card image cap">
-                        <div class="card-body">
-                            <h5 class="card-title">{{apartment.title}}</h5>
-                            <p class="card-text">{{apartment.description}}</p>
-                            <div>
-                                <span class="card-text">Stanze: {{apartment.rooms}}</span>
-                                <span class="card-text">Bagni: {{apartment.bathrooms}}</span>
-                                <span class="card-text">Mq: {{apartment.mq}}</span>
-                            </div>
-=======
                             <div class="text-center">
                                 <h4>Sponsorizzato</h4>
                             </div>
@@ -104,35 +65,17 @@
                             <span class="card-text">Stanze: {{apartment.rooms}}</span>
                             <span class="card-text">Bagni: {{apartment.bathrooms}}</span>
                             <span class="card-text">Mq: {{apartment.mq}}</span>
->>>>>>> cebbd33a4927e279b2c814ab9870c783eb109951
                         </div>
-                    </router-link>
-                </div>
-
-
+                    </div>
+                </router-link>
             </div>
-
         </div>
-<<<<<<< HEAD
-
-
-
-
-=======
->>>>>>> cebbd33a4927e279b2c814ab9870c783eb109951
     </div>
 </template>
 
 <script>
-    import axios from 'axios';
-    import SearchComp from './partials/SearchComp.vue';
-
-<<<<<<< HEAD
-    export default {
-        name: 'HomePage',
-        components: {
-            SearchComp,
-=======
+import axios from 'axios';
+import SearchComp from './partials/SearchComp.vue';
 export default {
     name: 'HomePage',
     components: {
@@ -161,7 +104,6 @@ export default {
                 .catch((error) => {
                     console.log(error)
                 });
-
             axios.get(`http://127.0.0.1:8000/api/apartments?title=${searchedText}`)
                 .then((res) => {
                 this.searchApartmentsArr = res.data.apartments;
@@ -170,31 +112,13 @@ export default {
                 .catch((error) => {
                     console.log(error)
                 });
->>>>>>> cebbd33a4927e279b2c814ab9870c783eb109951
         },
-        data() {
-            return {
-                apartmentsArr: [],
-                searchedApartmentsArr: [],
-                searchStatus: false,
-            }
+        search( searchedText ){
+            this.searchText = searchedText;
+            console.log(this.searchText);
+            this.searchStatus = true;
+            this.getApartments(this.searchText);
         },
-<<<<<<< HEAD
-        created() {
-            this.getApartments('a');
-        },
-        methods: {
-            getApartments(searchedText) {
-                axios.get(`http://127.0.0.1:8000/api/apartments`)
-                    .then((res) => {
-                        console.log(res.data);
-                        this.apartmentsArr = res.data.apartments;
-                        console.log('Appartamenti:', this.apartmentsArr);
-                    })
-                    .catch((error) => {
-                        console.log(error)
-                    });
-=======
         sponsoredApartments(){
             axios.get('http://127.0.0.1:8000/api/sponsored/apartments')
                 .then((res)=>{
@@ -210,33 +134,18 @@ export default {
                 })
         }
     },
->>>>>>> cebbd33a4927e279b2c814ab9870c783eb109951
-
-                axios.get(`http://127.0.0.1:8000/api/apartments?title=${searchedText}`)
-                    .then((res) => {
-                        this.searchApartmentsArr = res.data.apartments;
-                        console.log('Appartamenti cercati:', this.searchedApartmentsArr);
-                    })
-                    .catch((error) => {
-                        console.log(error)
-                    });
-            },
-            search(searchedText) {
-                this.searchText = searchedText;
-                console.log(this.searchText);
-                this.searchStatus = true;
-                this.getApartments(this.searchText);
-            },
-        },
-
-    }
-
+}
 </script>
 
-<style>
-    .marg {
-        margin-bottom: 0.8em;
-
+<style lang="scss" scoped>
+#container-apartments{
+    width: 100%;
+    .apartment-card{
+        width: calc(100%/10 * 2 - 1em);
+        margin: 0.5em 0.5em;
+        .card-body{
+            height: 25vh;
+        }
     }
-
+}
 </style>
