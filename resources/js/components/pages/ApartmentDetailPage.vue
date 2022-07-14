3727cf278f211eee73ac74868bfcbb5edebea37d
Simone Pavone <simonebest56@gmail.com>
2022-07-14 16:14:11 +0200

    Details Resolved

--- resources/js/components/pages/ApartmentDetailPage.vue
+++ resources/js/components/pages/ApartmentDetailPage.vue
@@ -1,23 +1,14 @@
 <template>
-    <div class="container">
+    <div>
         <!-- <SearchComp class="sticky-top" /> -->
+        <h2>{{ apartment.title }}</h2>
 
-<<<<<<< HEAD
-        <img :src="(`${apartment.image}`)" alt="img-apartment" class="img-fluid rounded">
-=======
         <span>{{ apartment.address }}</span>
         <img :src="(`../../../images/apartments/${apartment.image}`)" alt="img-apartment" class="w-50 rounded">
->>>>>>> cebbd33a4927e279b2c814ab9870c783eb109951
 
         <div class="row justify-content-between mt-4">
-            <div class="col-6 text-center">
-
-                <h2>{{ apartment.title }}</h2>
-
-                <span>Indirizzo: {{ apartment.address }}</span>
-
+            <div class="col-6">
                 <h3>{{ apartment.description }}</h3>
-
                 <div>
                     <!-- <span>{{ apartment.rooms }} camere - {{ apartment.beds }} letti - {{ apartment.bathrooms }} bagni - {{ apartment.mq }} mq</span> -->
 
@@ -38,11 +29,7 @@
 
                 <h3>Servizi</h3>
                 <ul>
-<<<<<<< HEAD
-                    <li v-for="(element) in apartment.services" :key="element.id">{{ element.name }}</li>
-=======
                     <li v-for="(element, index) in apartment.services" :key="index">{{ element.name }}</li>
->>>>>>> cebbd33a4927e279b2c814ab9870c783eb109951
                 </ul>
 
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia repellat eum sint earum aut itaque
@@ -55,7 +42,56 @@
                     ipsum dolor sit amet consectetur adipisicing elit. Ullam recusandae, dolore hic quo voluptas, sit
                     quod totam maxime animi provident vero rem porro libero consectetur cumque dolores consequatur
                     quidem facilis.
-                    
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia repellat eum sint earum aut itaque
+                    ratione pariatur harum temporibus delectus, cumque rem commodi nemo numquam omnis doloribus minus
+                    dolore expedita! Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi distinctio
+                    voluptates doloremque illum, consectetur nobis inventore, nostrum porro nisi laudantium unde
+                    eligendi explicabo voluptas sint debitis provident nemo ea ipsam?Lorem ipsum dolor sit amet
+                    consectetur, adipisicing elit. Iusto cupiditate vitae ad in officiis, hic optio fugiat molestiae,
+                    unde esse velit quam vero ipsum, nesciunt deleniti? Consectetur voluptas commodi molestiae!log Lorem
+                    ipsum dolor sit amet consectetur adipisicing elit. Ullam recusandae, dolore hic quo voluptas, sit
+                    quod totam maxime animi provident vero rem porro libero consectetur cumque dolores consequatur
+                    quidem facilis.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia repellat eum sint earum aut itaque
+                    ratione pariatur harum temporibus delectus, cumque rem commodi nemo numquam omnis doloribus minus
+                    dolore expedita! Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi distinctio
+                    voluptates doloremque illum, consectetur nobis inventore, nostrum porro nisi laudantium unde
+                    eligendi explicabo voluptas sint debitis provident nemo ea ipsam?Lorem ipsum dolor sit amet
+                    consectetur, adipisicing elit. Iusto cupiditate vitae ad in officiis, hic optio fugiat molestiae,
+                    unde esse velit quam vero ipsum, nesciunt deleniti? Consectetur voluptas commodi molestiae!log Lorem
+                    ipsum dolor sit amet consectetur adipisicing elit. Ullam recusandae, dolore hic quo voluptas, sit
+                    quod totam maxime animi provident vero rem porro libero consectetur cumque dolores consequatur
+                    quidem facilis.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia repellat eum sint earum aut itaque
+                    ratione pariatur harum temporibus delectus, cumque rem commodi nemo numquam omnis doloribus minus
+                    dolore expedita! Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi distinctio
+                    voluptates doloremque illum, consectetur nobis inventore, nostrum porro nisi laudantium unde
+                    eligendi explicabo voluptas sint debitis provident nemo ea ipsam?Lorem ipsum dolor sit amet
+                    consectetur, adipisicing elit. Iusto cupiditate vitae ad in officiis, hic optio fugiat molestiae,
+                    unde esse velit quam vero ipsum, nesciunt deleniti? Consectetur voluptas commodi molestiae!log Lorem
+                    ipsum dolor sit amet consectetur adipisicing elit. Ullam recusandae, dolore hic quo voluptas, sit
+                    quod totam maxime animi provident vero rem porro libero consectetur cumque dolores consequatur
+                    quidem facilis.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia repellat eum sint earum aut itaque
+                    ratione pariatur harum temporibus delectus, cumque rem commodi nemo numquam omnis doloribus minus
+                    dolore expedita! Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi distinctio
+                    voluptates doloremque illum, consectetur nobis inventore, nostrum porro nisi laudantium unde
+                    eligendi explicabo voluptas sint debitis provident nemo ea ipsam?Lorem ipsum dolor sit amet
+                    consectetur, adipisicing elit. Iusto cupiditate vitae ad in officiis, hic optio fugiat molestiae,
+                    unde esse velit quam vero ipsum, nesciunt deleniti? Consectetur voluptas commodi molestiae!log Lorem
+                    ipsum dolor sit amet consectetur adipisicing elit. Ullam recusandae, dolore hic quo voluptas, sit
+                    quod totam maxime animi provident vero rem porro libero consectetur cumque dolores consequatur
+                    quidem facilis.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia repellat eum sint earum aut itaque
+                    ratione pariatur harum temporibus delectus, cumque rem commodi nemo numquam omnis doloribus minus
+                    dolore expedita! Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi distinctio
+                    voluptates doloremque illum, consectetur nobis inventore, nostrum porro nisi laudantium unde
+                    eligendi explicabo voluptas sint debitis provident nemo ea ipsam?Lorem ipsum dolor sit amet
+                    consectetur, adipisicing elit. Iusto cupiditate vitae ad in officiis, hic optio fugiat molestiae,
+                    unde esse velit quam vero ipsum, nesciunt deleniti? Consectetur voluptas commodi molestiae!log Lorem
+                    ipsum dolor sit amet consectetur adipisicing elit. Ullam recusandae, dolore hic quo voluptas, sit
+                    quod totam maxime animi provident vero rem porro libero consectetur cumque dolores consequatur
+                    quidem facilis.
                 </p>
             </div>
 
@@ -73,55 +109,31 @@
 </template>
 
 <script>
-    // import SearchComp from './partials/SearchComp'
-    import axios from 'axios';
-
-    export default {
-        name: 'ApartmentDetailPage',
-
-        components: {
-            // SearchComp
+// import SearchComp from './partials/SearchComp'
+import axios from 'axios';
+export default {
+    name: 'ApartmentDetailPage',
+    components: {
+        // SearchComp
+    },
+    data() {
+        return {
+            apartment: [],
+            isError: false,
+        }
+    },
+    methods: {
+        getApartment() {
+            // il routing mi permette di usare $route che mi da determinate info (this.$route è parte di vue)
+            axios.get(`http://127.0.0.1:8000/api/apartments/${this.$route.params.id}`)
+                .then((res) => {
+                    console.log(res.data);
+                    this.apartment = res.data;
+                }).catch((err) => {
+                    console.log(err);
+                    this.isError = true;
+                });
         },
-<<<<<<< HEAD
-
-        data() {
-            return {
-                apartment: [],
-                isError: false,
-            }
-        },
-
-        methods: {
-            getApartment() {
-                // il routing mi permette di usare $route che mi da determinate info (this.$route è parte di vue)
-                axios.get(`http://127.0.0.1:8000/api/apartments/${this.$route.params.id}`)
-                    .then((res) => {
-                        console.log(res.data);
-                        this.apartment = res.data;
-                    }).catch((err) => {
-                        console.log(err);
-                        this.isError = true;
-                    });
-            },
-        },
-        mounted() {
-            this.getApartment();
-            console.log("this.$route");
-        },
-    }
-
-</script>
-
-<style scoped>
-    .prova_riquadro_prenota {
-        height: 500px;
-        width: 100%;
-        background-color: red;
-        position: sticky;
-        top: 50px;
-    }
-
-=======
     },
     created() {
         this.getApartment();
@@ -147,7 +159,6 @@
         
     },
 }
-
 </script>
 
 <style scoped>
@@ -158,10 +169,8 @@
     position: sticky;
     top: 50px;
 }
-
 .map {
     height: 400px;
     width: 35%;
 }
->>>>>>> cebbd33a4927e279b2c814ab9870c783eb109951
 </style>
