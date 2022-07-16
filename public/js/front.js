cea7c448958c51b2f350780f79c6e66ed669aa16
dibss <ludovicad01@gmail.com>
2022-07-16 17:37:09 +0200

    fix

--- public/js/front.js
+++ public/js/front.js
@@ -21223,11 +21223,10 @@ var router = new vue_router__WEBPACK_IMPORTED_MODULE_1__["default"]({
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-<<<<<<< HEAD
 module.exports = __webpack_require__(/*! C:\Users\Ludovica\Desktop\corso_boolean\esercizi\BoolBnB\resources\js\front.js */"./resources/js/front.js");
-=======
+
 module.exports = __webpack_require__(/*! C:\Users\franc\repo\BoolBnB-1\resources\js\front.js */"./resources/js/front.js");
->>>>>>> 0f9116c083c222d171d38df74d431e576c795b92
+
 
 
 /***/ })
