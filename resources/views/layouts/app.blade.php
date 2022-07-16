fa354a67b4467d5c1cad24a27d5d3890e1b7a7ec
Francesco De Sìmini <francescod995@gmail.com>
2022-07-16 19:04:49 +0200

    aggiunto link home sul logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,7 +29,9 @@
     <header>
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <img class="logo" src="{{ asset('images/logo.png') }}" alt="Boolbnb Logo">
+                <a href="/">
+                    <img class="logo" src="{{ asset('images/logo.png') }}" alt="Boolbnb Logo">
+                </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
